87c38e1383fa538176ccb35f78e88894c4d103b7
Chyzwar <schizek.marcin@gmail.com>
2015-09-14 21:31:49 +0100

    examples python

---
+++ Python/Excercises/generators_prime.py
@@ -0,0 +1,33 @@
+from math import sqrt
+# https://www.jeffknupp.com/blog/2013/04/07/improve-your-python-yield-and-generators-explained/
+
+
+def get_primes(input_list):
+    result_list = list()
+    for element in input_list:
+        if is_prime(element):
+            result_list.append()
+
+    return result_list
+
+# or better yet...
+
+
+def get_primes_comp(input_list):
+    return (element for element in input_list if is_prime(element))
+
+# not germane to the example, but here's a possible implementation of
+# is_prime...
+
+
+def is_prime(number):
+    if number > 1:
+        if number == 2:
+            return True
+        if number % 2 == 0:
+            return False
+        for current in range(3, int(sqrt(number) + 1), 2):
+            if number % current == 0:
+                return False
+        return True
+    return False
