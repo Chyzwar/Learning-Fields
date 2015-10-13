55f204a0c7a00e0d82928b2b41cd0d359264fdd7
Chyzwar <schizek.marcin@gmail.com>
2015-10-14 00:11:03 +0100

    added getter and seetter example py

--- Python/InterviewQuestions/jp-morgan/call-billing/src/callplan.py
+++ Python/InterviewQuestions/jp-morgan/call-billing/src/callplan.py
@@ -9,3 +9,19 @@ class CallPlan(object):
         self.regular_calls = tariff.get('regular_calls', 0.05)
         self.late_calls = tariff.get('late_calls', 0.02)
         self.weekend_calls = tariff.get('weekend_calls', 0.01)
+
+    @property
+    def x(self):
+        """I'm the 'x' property."""
+        print "getter of x called"
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        print "setter of x called"
+        self._x = value
+
+    @x.deleter
+    def x(self):
+        print "deleter of x called"
+        del self._x
