4d5a2f8d3cb7bf6c99ded6e675bf7c5dc71029e8
chyzwar <schizek.marcin@gmail.com>
2018-02-22 17:42:22 +0000

    read file

--- Elixir/cards/lib/cards.ex
+++ Elixir/cards/lib/cards.ex
@@ -10,11 +10,25 @@ defmodule Cards do
     end
   end
 
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {ok, binary } = File.read(filename)
+    :erlang.binary_to_term(binary)
   end
 end
