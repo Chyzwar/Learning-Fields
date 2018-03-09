d5dd73d5e34602dea7cbfcff7249da7f62b45e97
chyzwar <schizek.marcin@gmail.com>
2018-03-09 17:43:16 +0000

    cards

--- Elixir/cards/lib/cards.ex
+++ Elixir/cards/lib/cards.ex
@@ -28,11 +28,9 @@ defmodule Cards do
   end
 
   def load(filename) do
-    { status, binary } = File.read(filename)
-
-    case status do
-       :ok -> :erlang.binary_to_term(binary)
-       :error -> "stuff happens"
+    case  File.read(filename) do
+       {:ok, binary} -> :erlang.binary_to_term(binary)
+       {:error, :enoent} -> "This file do not exist"
     end
   end
 end
